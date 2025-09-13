bde150d8617c050129eb144e50670d1b60b6c31e
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-09-12 23:09:38 -0500

    > h

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
+// design
+import STICKY_design from "./design/header/header";
+
 const App = () => {
   return (
     <main>
-      <h1>Rsbuild with Preact</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <STICKY_design/>
     </main>
   );
 };
---
+++ src/design/header/header.tsx
@@ -0,0 +1,10 @@
+// STICKY_design
+const STICKY_design = () => {
+  return (
+    <main>
+      <h1>STICKY_design</h1>
+    </main>
+  );
+};
+
+export default STICKY_design;
