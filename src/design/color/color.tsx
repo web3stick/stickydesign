0039e47e30ae9208281c8639593bd6d71ee7fcd5
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-09-13 17:58:03 -0500

    c

--- src/design/color/color.tsx
+++ src/design/color/color.tsx
@@ -3,6 +3,7 @@ const STICKY_design_color = () => {
   return (
     <section>
       <h1>STICKY_color</h1>
+      <p>color palette for sticky</p>
     </section>
   );
 };
--- src/design/header.tsx
+++ src/design/header.tsx
@@ -3,6 +3,11 @@ const STICKY_design_header = () => {
   return (
     <header>
       <h1>STICKY_design</h1>
+      <p>
+        design system for sticky
+        <br/>
+        🐱 <a href="https://github.com/web3stick/stickydesign" target="_blank">github.com/web3stick/stickydesign</a>
+      </p>
     </header>
   );
 };
