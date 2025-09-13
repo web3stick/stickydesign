6d245d31319b8a27779b95368b6373e4eca239c4
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-09-13 16:31:47 -0500

    a

--- src/design/footer.tsx
+++ src/design/footer.tsx
@@ -1,8 +1,17 @@
+// import
+import { ROUTES } from "../ts/routes";
+
 // STICKY_design_footer
 const STICKY_design_footer = () => {
-
   return (
     <footer>
+      <nav>
+        {Object.values(ROUTES).map((route) => (
+          <a key={route.path} href={route.path}>
+            {route.label}
+          </a>
+        ))}
+      </nav>
       <p>copyright 2025 by sleet.near</p>
     </footer>
   );
--- src/ts/routes.ts
+++ src/ts/routes.ts
@@ -3,7 +3,7 @@ export const ROUTES = {
     home: { path: '/', label: 'HEADER' },
     color: { path: '/color', label: 'COLOR' },
     button: { path: '/button', label: 'BUTTON' },
-    footer: { path: '/footer', label: 'FOOTER' },
+    // footer: { path: '/footer', label: 'FOOTER' },
   } as const;
   
   export type RouteKey = keyof typeof ROUTES;
