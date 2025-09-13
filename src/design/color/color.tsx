fc1641a63cf7df69d8edbcd3c7ff61c404358578
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-09-13 18:03:20 -0500

    c

--- src/design/color/color.tsx
+++ src/design/color/color.tsx
@@ -1,9 +1,22 @@
+
+// import css
+import "./color.css";
+import button_CSS from "./color.css?raw";
+
 // STICKY_design_color
 const STICKY_design_color = () => {
   return (
     <section>
       <h1>STICKY_color</h1>
       <p>color palette for sticky</p>
+      <div class="color_palette">
+        <div class="color_palette_color_one"></div>
+        <div class="color_palette_color_two"></div>
+        <div class="color_palette_color_three"></div>
+        <div class="color_palette_color_four"></div>
+        <div class="color_palette_color_five"></div>
+        <div class="color_palette_color_six"></div>
+      </div>
     </section>
   );
 };
