8b818f5627c1c2fd6da13d1ce5b9d5b3cb7d2443
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-09-16 12:10:34 -0500

    Update near_auth_button.tsx

--- src/design/near/near_auth_button.tsx
+++ src/design/near/near_auth_button.tsx
@@ -1,12 +1,37 @@
+import { useEffect } from "preact/hooks";
+import Prism from "prismjs";
+import "prismjs/themes/prism-okaidia.css";
+
 // near tsx
 import NEAR_AUTH_BUTTON from "./auth_button/near_auth_button";
+import NEAR_AUTH_BUTTON_RAW_TSX from "./auth_button/near_auth_button.tsx?raw";
+import NEAR_AUTH_BUTTON_RAW_TS from "./auth_button/near.auth.ts?raw";
+
+
 
 // STICKY_design_near_auth_button
 const STICKY_design_near_auth_button = () => {
+  
+  // Prism
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+  
   return (
     <section>
       <h1>NEAR AUTH BUTTON</h1>
       <NEAR_AUTH_BUTTON />
+      
+      
+      {/*pre*/}
+      <p>BUTTON TSX</p>
+      <pre>
+        <code className="language-js">{NEAR_AUTH_BUTTON_RAW_TSX}</code>
+      </pre>
+      <p>BUTTON TS</p>
+      <pre>
+        <code className="language-js">{NEAR_AUTH_BUTTON_RAW_TS}</code>
+      </pre>
 
     </section>
   );
