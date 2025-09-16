8b46e949ccdf45d172e59522422960eb34da3bee
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-09-16 09:18:04 -0500

    css

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { ROUTES } from "./ts/routes.ts";
 
 
 // design
+import STICKY_design_nav from "./design/nav_header.tsx";
 import STICKY_design_header from "./design/header.tsx";
 import STICKY_design_color from "./design/color/color.tsx";
 import STICKY_design_button from "./design/button/button.tsx";
@@ -21,6 +22,7 @@ import STICKY_design_footer from "./design/footer.tsx";
 const App = () => {
   return (
     <main>
+      <STICKY_design_nav/>
       <Router>
         <Route path={ROUTES.home.path} component={STICKY_design_header} />
         <Route path={ROUTES.color.path} component={STICKY_design_color} />
--- src/design/nav_header.tsx
+++ src/design/nav_header.tsx
@@ -2,12 +2,7 @@
 const STICKY_design_nav = () => {
   return (
     <nav>
-      <h1>STICKY_design</h1>
-      <p>
-        design system for sticky
-        <br/>
-        🐱 <a href="https://github.com/web3stick/stickydesign" target="_blank">github.com/web3stick/stickydesign</a>
-      </p>
+      <h4>STICKY_design</h4>
     </nav>
   );
 };
