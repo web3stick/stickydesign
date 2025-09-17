2c449d56a9b50508af2b27db1a76603ed8e71f8f
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-09-17 10:30:28 -0500

    Update button_page.tsx

--- src/design/basic/button/button_page.tsx
+++ src/design/basic/button/button_page.tsx
@@ -6,6 +6,7 @@ import "prismjs/themes/prism-okaidia.css";
 import "./button.css";
 import button_CSS from "./button.css?raw";
 import STICKY_button from "./button_componenet";
+import button_Component from "./button_componenet?raw";
 
 // STICKY_design_button_page
 const STICKY_design_button_page = () => {
@@ -21,7 +22,26 @@ const STICKY_design_button_page = () => {
       {/*STICKY_button*/}
       <STICKY_button text="STICKY_button" />
 
-      {/*pre*/}
+      <h2>How to Reuse</h2>
+      <p>
+        The STICKY_button component can be easily reused throughout your application.
+        <br/>
+        Simply import it and pass the desired text as a prop:
+      </p>
+      
+      <pre>
+        <code className="language-js">{`import STICKY_button from "@src/design/basic/button/button_componenet";
+
+// Usage
+<STICKY_button text="Click Me" />`}</code>
+      </pre>
+
+      <h2>Component Code</h2>
+      <pre>
+        <code className="language-js">{button_Component}</code>
+      </pre>
+
+      <h2>CSS Code</h2>
       <pre>
         <code className="language-css">{button_CSS}</code>
       </pre>
