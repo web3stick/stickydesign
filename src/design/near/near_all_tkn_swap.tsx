e56d2a49bc74a8495dff2ff9fa1045859ed264e3
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-09-16 22:20:10 -0500

    Update near_all_tkn_swap.tsx

--- src/design/near/near_all_tkn_swap.tsx
+++ src/design/near/near_all_tkn_swap.tsx
@@ -6,6 +6,7 @@ const STICKY_design_near_all_tkn_swap = () => {
   return (
     <section>
       <h1>TOKEN SWAP COMPONENT</h1>
+      <p>working on making this reusable</p>
       <NEAR_ALL_TOKEN_Swap_FULL_COMPONENT />
 
     </section>
