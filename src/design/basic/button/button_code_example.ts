8535ac83e28d625f5825fd4a6f28afb8a0f8a38e
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-09-17 10:46:24 -0500

    buton exam

---
+++ src/design/basic/button/button_code_example.ts
@@ -0,0 +1,4 @@
+export const buttonExample = `import STICKY_button from "@src/design/basic/button/button_componenet";
+
+// Usage
+<STICKY_button text="Click Me" />`;
--- src/design/basic/button/button_page.tsx
+++ src/design/basic/button/button_page.tsx
@@ -7,6 +7,7 @@ import "./button.css";
 import button_CSS from "./button.css?raw";
 import STICKY_button from "./button_componenet";
 import button_Component from "./button_componenet?raw";
+import { buttonExample } from "./button_code_example";
 
 // STICKY_design_button_page
 const STICKY_design_button_page = () => {
@@ -18,22 +19,19 @@ const STICKY_design_button_page = () => {
     <section>
       <h1>STICKY_button</h1>
       <p>button for sticky</p>
-      
+
       {/*STICKY_button*/}
       <STICKY_button text="STICKY_button" />
 
       <h2>How to Reuse</h2>
       <p>
         The STICKY_button component can be easily reused throughout your application.
-        <br/>
+        <br />
         Simply import it and pass the desired text as a prop:
       </p>
-      
-      <pre>
-        <code className="language-js">{`import STICKY_button from "@src/design/basic/button/button_componenet";
 
-// Usage
-<STICKY_button text="Click Me" />`}</code>
+      <pre>
+        <code className="language-js">{buttonExample}</code>
       </pre>
 
       <h2>Component Code</h2>
